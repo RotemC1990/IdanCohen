c5983738a6d9fa822bad74d281e1fd844aea92c2
RotemC1990 <Astree1990@gmail.com>
2020-09-14 18:36:15 +0300

    home view repair

--- src/app/publicView/home/home.component.ts
+++ src/app/publicView/home/home.component.ts
@@ -11,19 +11,22 @@ import { Video } from 'src/app/models/video.model';
 export class HomeComponent implements OnInit {
    videoSrcArray: string[] = ["../../assets/videos/Commercial/DutyFree-Lindt.mp4","../../assets/videos/Events/H.I.TGraduateExhbition2017-VisualCommunication.mp4",
                               "../../assets/videos/Events/Purim_2019_EventTAU.mp4","../../assets/videos/Events/Vayyar-CES_2020.mp4",
-                              "../../assets/videos/Events/VayyarOnEcomotionTLV2019.mp4"];
+                              "../../assets/videos/Events/VayyarOnEcomotionTLV2019.mp4","../../assets/videos/Commercial/DutyFree-Loreal.mp4"];
     posterSrcArray = ["../../assets/posters/lindt.jpg",
                       "../../assets/posters/H.I.TGraduateExhbition2017-VisualCommunication.jpg",
                       "../../assets/posters/Purim_2019_EventTAU.jpg",
                       "../../assets/posters/Vayyar-CES_2020.jpg",
-                      "../../assets/posters/VayyarOnEcomotionTLV2019.jpg"];
+                      "../../assets/posters/VayyarOnEcomotionTLV2019.jpg",
+                      "../../assets/posters/loreal.jpg"];
  videoNameArray: string[] = ["DutyFree - Lindt","H.I.T Graduate Exhbition 2017 - Visual Communication","Purim_2019_EventTAU","Vayyar - CES_2020",
-                            "Vayyar On Ecomotion TLV 2019"];
- videoDescArray: string[] = ["1111111","22222222","33333333","444444444","5555555"];
+                            "Vayyar On Ecomotion TLV 2019", "Duty Free - Loreal"];
+ videoDescArray: string[] = ["1111111","22222222","33333333","444444444","5555555","666666"];
 
    
 
  videos : Video[]=[];
+ videoRows : number =0;
+ trEndStart :string = "</td><tr></tr><td>";
   //videos: MediaViewerComponent[];
   constructor(/*mediaViewer: MediaViewerComponent*/) { }
   
@@ -31,12 +34,15 @@ export class HomeComponent implements OnInit {
     for(let i=0;i<this.videoDescArray.length;i++){
       this.videos[i] = new Video(this.videoSrcArray[i],this.posterSrcArray[i],this.videoNameArray[i],this.videoDescArray[i],false);
     }
-  
+    
+    this.videoRows= this.videos.length/3;
 
   }
 
   openModal(videoSelected: Video) {
     console.log(videoSelected)
+
+    console.log("videos num div by 3 is " + this.videoRows)
     
                             
   }
